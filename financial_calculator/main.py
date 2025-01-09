c8806e8c7bb9279d690f7416dd615ef718c52224
pedroesouza <pedro.souza@ucas-edu.net>
2025-01-09 13:44:52 -0700

    addedcd functions

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -2,7 +2,26 @@
 
 #Pseudocode
 
-def main():
+def calc_tip():
     pass
 
-#comment
+def calc_price():
+    pass
+
+def budget():
+    pass
+
+def calc_compound():
+    pass
+
+def calc_goal():
+    input("How much is your monthly deposit?")
+    input("What is your goal?")
+
+def main():
+    choice = input("\nWould you like to\n1. Calulate length of saving for goal\n2. Calculate compund interest\n3. Get budget allocation help\n4. Calculate discount and tax slae price\n5. Calculate tip")
+
+    if choice == 1:
+        calc_goal()
+
+main()
