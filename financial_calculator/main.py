8f7dd8385439114d1dd42af41fde2f2f60e266c2
pedroesouza <pedro.souza@ucas-edu.net>
2025-01-12 22:22:14 -0700

    Update main.py

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -6,15 +6,19 @@
 #3. Get budget allocation help
 #4. Calculatdiscount and tax slae price
 #5. Calculate tip
-e 
+
 def calc_tip():
-    pass
+    ogPrice = input("What was the original price?")
+    taxPercent = input(f"What % of tax is in your state?")
+    finalPrice = ogPrice * (taxPercent+1)
+    return finalPrice
 
 def calc_price():
     pass
 
 def budget():
-    input("What up gang, how much money are you willing to spend rn gang fan dude guy man dude man guy dude man dude ma guy fan gand bro fam bestie buddy bucko pal gang fam dude fan gant Im literally bald")
+    input("How much money do you have this month?")
+    print("30 percent rent 10 percent food, ten percent fun, 20 percent savings, and the leftover 30 percent should go to medical bills, debt, religious offerings, charity, or any other thing")
 
 def calc_compound():
     pass
@@ -23,11 +27,24 @@ def calc_goal():
     deposit = input("How much is your monthly deposit?")
     goal = input("What is your goal?")
 
+    try:
+        goal = int(goal)
+    except:
+        goal = input("ERROR. What is your goal, please only answer with integers?")
+        
+    try:
+        deposit = int(deposit)
+    except:
+        deposit = input("ERROR. How much is your monthly deposit, please only answer with integers?")
+
+    totalTime = (goal // deposit) + 1
+    return totalTime
+
 def main():
     choice = input("\nWould you like to\n1. Calulate length of saving for goal\n2. Calculate compund interest\n3. Get budget allocation help\n4. Calculate discount and tax slae price\n5. Calculate tip\n")
 
     if choice == "1":
-        calc_goal()
+        print(f"It will take you {calc_goal()} months to complete your assignment.")
 
     if choice == "2":
         calc_compound()
