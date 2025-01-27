09b073f74de59731320583b72a335fc5642ea6d2
pedroesouza <pedro.souza@ucas-edu.net>
2025-01-26 23:20:32 -0700

    Update main.py

--- random_password_generator/main.py
+++ random_password_generator/main.py
@@ -15,24 +15,42 @@ special_characters = [
 
 password = ""
 
-def make_password(amount):
-    func_password = ""
-    for i in range(1, int(amount) + 1):
-        rng = random.choice([1, 2, 3, 4])
-        if rng == 1:
-            func_password += random.choice(lowercase_letters)
-        if rng == 2:
-            func_password += random.choice(uppercase_letters)
-        if rng == 3:
-            func_password += random.choice(numbers)
-        if rng == 4:
-            func_password += random.choice(special_characters)
+def make_password(amount, uppercase, num, specialChar):
+    funcPassword = ""
+    
+    hasUp = False
+    hasNum = False
+    hasSpecChar = False
 
-    return func_password
+    fails = 0
+
+    if uppercase == "y":
+        rngList += 2
+
+    if num == "y":
+        rungList += 3
+
+    if specialChar == "y":
+        rngList += 4
+
+    for x in range (4+fails):
+        for y in range(1, int(amount) + 1):
+            rng = random.choice(rngList)
+            if rng == 1:
+                funcPassword += random.choice(lowercase_letters)
+            if rng == 2:
+                funcPassword += random.choice(uppercase_letters)
+            if rng == 3:
+                funcPassword += random.choice(numbers)
+            if rng == 4:
+                funcPassword += random.choice(special_characters)
+            
+            for z in range (1, funcPassword):
+
+                if z in uppercase:
+                    hasUp = True
 
 def main():
-    main_password = make_password(input("How many characters? "))
-    return main_password
+    make_password(input("How many characters? "), input("Would you like to include uppercase letters?"), input("Would you like numbers?"), input("Would you like special characters?"))
 
 password = main()
-print(password)
