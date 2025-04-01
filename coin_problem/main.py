3d532966abd62b72484f09b25f140a24750468d0
pedroesouza <pedro.souza@ucas-edu.net>
2025-04-01 05:41:23 +0000

    message

--- coin_problem/main.py
+++ coin_problem/main.py
@@ -1,4 +1,18 @@
-def main():
-    pass
+#Pedro Elias Souza, Coin change problem
 
-main()
+#Main file/function just has the starting UI and choice management
+
+#Imports the file and function with the most logic
+from coin_change_manager import coin_change_main as coin_change
+
+def main():
+    start = input("Would you like to convert currency(1) or exit(2): ")
+    if start == "1":
+        coin_change()
+    elif start == "2":
+        exit()
+    else:
+        print("Invalid input!")
+
+while True:
+    main()
