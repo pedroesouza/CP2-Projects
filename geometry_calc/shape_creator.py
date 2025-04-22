22d2c6ec0928a9b26f70ca511dbf4129939a9f8d
pedroesouza <pedro.souza@ucas-edu.net>
2025-04-21 22:41:47 -0600

    Update shape_creator.py

--- geometry_calc/shape_creator.py
+++ geometry_calc/shape_creator.py
@@ -4,7 +4,7 @@
 from shape import Circle, Square, Rectangle, Triangle
 from csv_manager import save_shapes
 
-#Function creates the shape and sets up csv manager to save it
+#Function creates the shape and sets up CSV manager to save it
 def create_shape(shapes):
     #Asks for name and shape type, making sure that the name is unique
     shape_type = input("Enter shape type (circle/square/rectangle/triangle): ").strip().lower()
@@ -16,13 +16,13 @@ def create_shape(shapes):
         #Ask user for info on the shape
         if shape_type == 'circle':
             radius = float(input("Enter radius: "))
-            if radius > 0: #This and others like this just make sure the number is valid
+            if radius > 0:
                 shapes.append(Circle(name, radius))
             else:
                 print("No negatives or zero's allowed")
         elif shape_type == 'square':
             side = float(input("Enter side length: "))
-            if radius > 0:
+            if side > 0:
                 shapes.append(Square(name, side))
             else:
                 print("No negatives or zero's allowed")
@@ -34,16 +34,18 @@ def create_shape(shapes):
             else:
                 print("No negatives or zero's allowed")
         elif shape_type == 'triangle':
-            base = float(input("Enter base: "))
+            side1 = float(input("Enter side 1 length: "))
+            side2 = float(input("Enter side 2 length: "))
+            side3 = float(input("Enter side 3 length: "))
             height = float(input("Enter height: "))
-            if base > 0 and height > 0:
-                shapes.append(Triangle(name, base, height))
+            if side1 > 0 and side2 > 0 and side3 > 0 and height > 0:
+                shapes.append(Triangle(name, side1, side2, side3, height))
             else:
                 print("No negatives or zero's allowed")
-        else: #Error managing idiot proofing
+        else:
             print("Invalid shape type.")
             return
-        save_shapes(shapes) #call the function to save the shapes on csv
+        save_shapes(shapes)  #Save the new shapes to CSV
         print("Shape created successfully.")
     except:
-        print("Invalid answer, not a number.") #For when the number is not a number
+        print("Invalid answer, not a number.")  #Handle invalid input
